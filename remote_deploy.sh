588a82ff66bd2ec121e5fb4bf4921ccafae38feb
Alex Smith <alexsmith1@me.com>
2020-04-03 16:11:52 -0400

    remote deploy script

---
+++ remote_deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+
+source .remote-deploy.env
+
+ssh -T $REMOTE_DEPLOY_USER@$REMOTE_DEPLOY_HOST <<EOF
+set -e
+cd timesheet
+echo '>>> UPDATING SOURCES'
+git fetch
+git reset --hard origin/master
+echo '>>> INSTALLING DEPENDENCIES'
+sudo apt-get install uwsgi -y
+sudo apt-get install uwsgi-plugin-python3 -y
+python3 -m venv .
+. bin/activate
+pip install -r requirements.txt
+deactivate
+echo '>>> RELOAD SYSTEMD UWSGI PROCESS'
+sudo cp timesheet.service /etc/systemd/system/timesheet.service
+sudo systemctl daemon-reload
+sudo systemctl restart timesheet
+sudo systemctl enable timesheet
+EOF
+
+echo '>>> DEPLOY COMPLETE'
+
